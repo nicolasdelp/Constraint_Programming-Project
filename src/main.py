fa6830b741391089b181fd41f14077c08ac2090a
Delplanque Nicolas <delplanque.n@outlook.com>
2023-01-26 11:17:03 +0100

    First test

--- src/main.py
+++ src/main.py
@@ -1,2 +1,26 @@
 from pycsp3 import *
 
+# Instance didactique
+n = 5
+
+
+
+# Nos étiquettes
+etiq = [i+1 for i in range(n)] 
+
+# Variables de décision
+
+e = VarArray(size=n, dom=etiq) # Domaine
+print("Domain of any variable: ", e[0].dom)
+
+# Contraintes
+
+satisfy(
+    AllDifferent(e)
+);
+
+# Fonction objective
+minimize(
+    Maximum( min( abs( e[i]-e[j], n - abs( e[i]-e[j] )) for i,j in n))
+)
+print(objective())
