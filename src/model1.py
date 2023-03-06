1a77cbb89587b74a88ccd65d3b21ac76b74f9c49
nacimchafaa <nacimchafaa@gmail.com>
2023-03-06 09:30:41 +0100

    last update

--- src/model1.py
+++ src/model1.py
@@ -2,22 +2,15 @@ from pycsp3 import *
 
 n, E, edges = data
 
-
-# Nos étiquettes
-etiq = [i+1 for i in range(n)] 
-
 # Variables de décision
-
-e = VarArray(size=n, dom=etiq) # Domaine
-print("Domain of any variable: ", e[0].dom)
+x = VarArray(size=n, dom=range(n))
 
 # Contraintes
-
 satisfy(
-    AllDifferent(e)
+    AllDifferent(x)
 )
 
 # Fonction objectif
 minimize(
-    Maximum( min( abs( e[i]-e[j], n - abs( e[i]-e[j] )) for i,j in n))
+   Maximum(Minimum(abs(x[u] - x[v]), n - abs(x[u] - x[v])) for u, v in edges)
 )
