bdb0b4305220e28838d6c1b05fdeda41dd609397
Delplanque Nicolas <delplanque.n@outlook.com>
2023-03-24 17:32:29 +0100

    Teacher feedback

---
+++ src/model3.py
@@ -0,0 +1,9 @@
+
+# V = nombre de sommets | E = nombre d'arêtes | edges = les arètes
+V, E, edges = data
+
+# TODO
+# Utiliser la table inverse pour être en CNF
+# Trouver une propriété pour optimiser OU dicotomie
+# Mettre le solveur dans le model
+# py model1.py -data="..." -model=""
