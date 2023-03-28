f940e5e7e9b1c38e3c5ddd5e314a93baaca08330
nacimchafaa <nacimchafaa@gmail.com>
2023-03-28 13:08:39 +0200

    Last update

--- src/model2.py
+++ src/model2.py
@@ -3,20 +3,27 @@ from pycsp3 import *
 # V = nombre de sommets | E = nombre d'arêtes | edges = les arètes
 V, E, edges = data
 
-# Valeur par défaut du cyclic-bandwidth
+# Valeur par défaut du cyclic-bandwidth (largeur max de la bande cyclique)
 k = 10 
 
 # Variables de décision
 x = VarArray(size=V, dom=range(V))
 
+# Table de contraintes pour le problème. Cette table contient tous les paires (i, j) 
+# de sommets tels que la distance cyclique entre i et j (c'est-à-dire la distance minimale sur le cycle)
+# est inférieure ou égale à k. Cette table est utilisée dans la contrainte 2
 table = {
     (i, j) for i in range(V) 
                 for j in range(V)
                     if min(abs(i - j), V - abs(i - j)) <= k and i != j
         }
 
+
 satisfy(
+    # Assure que chaque variable x[i] est différente des autres variables x[j]
     AllDifferent(x),
+    # Vérifie que toutes les arêtes du graphe (représentées par la liste d'arêtes "edges") sont telles que
+    # la paire (x[i], x[j]) est présente dans la table de contraintes définie précédemment.
     [(x[i], x[j]) in table for i, j in edges]
-    (x[0] == 0)  # on peut fixer la position du premier sommet à 0 sans perte de généralité
+    
 )
--- src/model3.py
+++ src/model3.py
@@ -1,7 +1,51 @@
-from pycsp3 import *
+from pysat.solvers import Glucose4
+from pysat.formula import CNF
 
 # V = nombre de sommets | E = nombre d'arêtes | edges = les arètes
 V, E, edges = data
+
+# Valeur par défaut du cyclic-bandwidth
+k = 10 
+
+# Variables booléennes
+x = [[ Var(id=f"x_{i}_{j}", dom=[False, True]) for j in range(V)] for i in range(V)]
+
+# Contrainte de présence
+cnf = CNF()
+for i in range(V):
+    clause = [x[i][j] for j in range(V)]
+    cnf.append(clause)
+
+# Contrainte de différence
+for i in range(V):
+    for j in range(i+1, V):
+        for k in range(V):
+            clause1 = [-x[i][k], -x[j][(k + i - j) % V]]
+            clause2 = [-x[j][k], -x[i][(k + j - i) % V]]
+            cnf.append(clause1)
+            cnf.append(clause2)
+
+# Contrainte de bande passante cyclique
+for (i, j) in edges:
+    for d in range(-k, k+1):
+        if d != 0:
+            clause = [-x[i][(j + d) % V], -x[j][(i + d) % V]]
+            cnf.append(clause)
+
+# Résolution du problème en utilisant Glucose 4
+with Glucose4(bootstrap_with=cnf) as solver:
+    if solver.solve():
+        model = solver.get_model()
+        solution = [0] * V
+        for i in range(V):
+            for j in range(V):
+                if model[x[i][j]-1] > 0:
+                    solution[j] = i
+        print("Solution:", solution)
+    else:
+        print("Le problème est insatisfaisable")
+
+
 
 # TODO
 # Utiliser la table inverse pour être en CNF
