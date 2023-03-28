1c5c5b67ccfab663726e814b566821560ac5d3e7
nacimchafaa <nacimchafaa@gmail.com>
2023-03-28 15:22:19 +0200

    Last one

---
+++ src/model1+.py
@@ -0,0 +1,18 @@
+from pycsp3 import *
+
+# V = nombre de sommets | E = nombre d'arêtes | edges = les arètes
+V, E, edges = data
+
+# Variables de décision
+x = VarArray(size=V, dom=range(V))
+
+# Contraintes en éliminant les symétries
+satisfy(
+    x[0] == 1 
+    x[2] < x[V-1],
+    AllDifferent(x)
+)
+
+minimize(
+   Maximum(Minimum(abs(x[u] - x[v]), V - abs(x[u] - x[v])) for u, v in edges)
+)
