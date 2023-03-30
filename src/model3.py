96eec3d1be16a7e2f0b2ff129b4af1ad47c22e41
nacimchafaa <nacimchafaa@gmail.com>
2023-03-31 00:01:54 +0200

    Modèle 3

--- src/model3.py
+++ src/model3.py
@@ -1,14 +1,10 @@
-from pysat.solvers import Glucose4
-from pysat.formula import CNF
 import json
+import time
 import sys
-
-# Choix du solveur
-solver = Glucose4()
+from pysat.solvers import Glucose4
 
 # Lecture des données depuis le fichier Json
 data = json.load(open(sys.argv[2], 'r'))
-
 V = data['V']
 E = data['E']
 edges = data['edges']
@@ -16,73 +12,53 @@ edges = data['edges']
 # Lecture de la valeur de k
 k = int(sys.argv[1])
 
-def cyclic_bandwidth_problem(n, E, edges, k):
+# Début du timer
+start = time.time()
 
-    # Création de la formule CNF
-    cnf = CNF()
-    
-    # Création des variables booléennes
-    variables = {}
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            variables[(i, j)] = len(variables) + 1
-    
-    # Contrainte 1 : T est toujours vrai
-    cnf.append([variables[(1, 1)]])
-    
-    # Contrainte 2 : Au moins une étiquette pour chaque sommet
-    for i in range(1, n+1):
-        clause = []
-        for j in range(1, n+1):
-            clause.append(variables[(i, j)])
-        cnf.append(clause)
-    
-    # Contrainte 3 : Deux sommets éloignés de plus de k ne peuvent pas avoir la même étiquette
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            for h in range(1, n+1):
-                if h != i:
-                    for d in range(1, k+1):
-                        if j-d >= 1:
-                            cnf.append([-variables[(i, j)], -variables[(h, j-d)]])
-                        if j+d <= n:
-                            cnf.append([-variables[(i, j)], -variables[(h, j+d)]])
-    
-    # Contrainte 4 : Deux sommets adjacents ne peuvent pas avoir des labels incompatibles
-    for (u, v) in E:
-        for i in range(1, n+1):
-            for j in range(1, n+1):
-                if not edges[u][v][(i-1)%n][(j-1)%n]:
-                    cnf.append([-variables[(u, i)], -variables[(v, j)]])
-    
-    # Contrainte 5 : Éviter les contraintes redondantes
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            for k in range(1, n+1):
-                if k != j:
-                    cnf.append([-variables[(i, j)], -variables[(i, k)]])
-    
-    # Ajout de la variable y pour la satisfaction des contraintes
-    y = len(variables) + 1
-    cnf.append([y])
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            cnf.append([-y, variables[(i, j)]])
-    
-    # Création du solveur et résolution du problème
-    solver = Glucose4()
-    solver.append_formula(cnf)
-    if solver.solve():
-        print("La contrainte de cyclic-bandwidth k =", k, "est satisfaite")
-        # Récupération de la solution
-        solution = []
-        for i in range(1, n+1):
-            for j in range(1, n+1):
-                if solver.model[variables[(i, j)]-1] > 0:
-                    solution.append((i, j))
-        print("Sat")
-    else:
-        print("La contrainte est insatisfaite")
+# Créer un solveur SAT
+solver = Glucose4()
 
-e = cyclic_bandwidth_problem(V, E, edges, k)
-print(e)
+# Créer une variable booléenne pour chaque permutation possible des sommets
+perms = {}
+for i in range(V):
+    for j in range(V):
+        perms[(i, j)] = solver.nof_vars()
+
+# Contraintes de domaine: chaque variable doit être soit vraie, soit fausse
+for i in range(V):
+    for j in range(V):
+        solver.add_clause([perms[(i, j)], -perms[(i, j)]])
+
+# Contraintes de cycle: chaque sommet doit apparaître exactement une fois dans chaque cycle
+for i in range(V):
+    cycle_vars = []
+    for j in range(V):
+        cycle_vars.append(perms[(i, j)])
+    solver.add_clause(cycle_vars)
+
+# Contraintes de largeur de bande: pour chaque arête, la différence entre les positions de ses extrémités
+# doit être inférieure ou égale à k
+for edge in edges:
+    u, v = edge
+    for i in range(V):
+        for j in range(V):
+            if i < j:
+                if (i == u and j == v) or (i == v and j == u):
+                    continue
+                # Si les deux sommets ne sont pas voisins dans l'arête, leur différence doit être supérieure à k
+                if abs(i - j) > k + 1:
+                    solver.add_clause([-perms[(i, u)], -perms[(j, v)]])
+                    solver.add_clause([-perms[(i, v)], -perms[(j, u)]])
+                # Sinon, leur différence doit être inférieure ou égale à k
+                else:
+                    solver.add_clause([perms[(i, u)], perms[(j, v)]])
+                    solver.add_clause([perms[(i, v)], perms[(j, u)]])
+
+# Vérifier si le problème est satisfaisable
+if solver.solve():
+    print("Le problème est satisfaisable.")
+else:
+    print("Le problème est insatisfaisable.")
+
+end = time.time()
+print("Temps d'execution :", end - start)
