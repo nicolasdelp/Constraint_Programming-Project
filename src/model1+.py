df4363e20b6d19f38f5a70545322102e91677af0
nacimchafaa <nacimchafaa@gmail.com>
2023-04-01 11:08:37 +0200

    maj

--- src/model1+.py
+++ src/model1+.py
@@ -8,7 +8,9 @@ x = VarArray(size=V, dom=range(V))
 
 # Contraintes (avec élimination des symétries)
 satisfy(
+    # fixe la position du premier sommet à 1
     x[0] == 1,
+    # élimine les solutions symétriques dans lesquelles le premier sommet est placé à la position V-1 et le troisième sommet est placé à la position 2
     x[2] < x[V-1],
     AllDifferent(x)
 )
--- src/model2+.py
+++ src/model2+.py
@@ -23,7 +23,9 @@ table = {
 
 # Contraintes (avec élimination des symétries)
 satisfy(
+    # fixe la position du premier sommet à 1
     x[0] == 1,
+    # élimine les solutions symétriques dans lesquelles le premier sommet est placé à la position V-1 et le troisième sommet est placé à la position 2
     x[2] < x[V-1],
     AllDifferent(x),
     [(x[u], x[v]) in table for u, v in edges]
